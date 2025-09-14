c36cc491a8255a72f517463a1ec3209146795d6b
Vladyslav Tsalko <vladyslav.tsalko77@gmail.com>
2025-09-14 15:57:38 +0200

    added dockerfile and changed allowed origins

--- tetris-backend/src/main/java/com/vladyslav_tsalko/tetris_backend/config/WebConfig.java
+++ tetris-backend/src/main/java/com/vladyslav_tsalko/tetris_backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5501")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
